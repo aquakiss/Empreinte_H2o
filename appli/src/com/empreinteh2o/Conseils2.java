763a07eb5ad1dcabde4a46b2e6cc0a4e51733fc3
Chaoskira <fredverio@hotmail.fr>
2014-02-05 10:22:48 +0100

    ajout boutons page conseils

--- appli/src/com/empreinteh2o/Conseils2.java
+++ appli/src/com/empreinteh2o/Conseils2.java
@@ -2,7 +2,11 @@ package com.empreinteh2o;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Conseils2 extends Activity {
 
@@ -10,9 +14,32 @@ public class Conseils2 extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_conseils2);
+	
+	
+		Button changeButton = (Button) findViewById(R.id.button1);
+	    changeButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Intent changeActivity = new Intent(getApplicationContext(), MainActivity.class);
+				startActivity(changeActivity);
+			}
+		});
+	    
+	    Button changeButton2 = (Button) findViewById(R.id.button2);
+	    changeButton2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Intent changeActivity = new Intent(getApplicationContext(), Graphe.class);
+				startActivity(changeActivity);
+			}
+		});
+	
+	
+	
+	
 	}
 
-		
+	
 	
 	
 	@Override
